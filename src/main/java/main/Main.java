9b143cc3d1f14986e6af86f80c438dec4a14b28c
Roy-Cheong <roy8289@gmail.com>
2025-04-21 03:27:57 +0100

    feat: load subject screen on app launch

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,19 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("AI Study Planner is alive!");
-        primaryStage.setScene(new Scene(label, 400, 200));
-        primaryStage.setTitle("AI Study Planner");
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/subject_screen.fxml"));
+        Scene scene = new Scene(loader.load(), 400, 450);
+        stage.setTitle("AI Study Planner - Subjects");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
